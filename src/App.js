54a4f80c0c117a28834629e1f5b5901dbd90c087
fortunato-sply <lucasfortunato2530@gmail.com>
2024-03-23 15:13:40 -0300

    Branch da faculdade

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+  
+  const dbEmail = 'lucas.fortunato@pucpr.com';
+  const dbPassword = '123';
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleOnClick = () => {
+    if(email === dbEmail && password === dbPassword)
+      setMessage('Acessado com sucesso!')
+    else
+      setMessage('Email ou senha incorretos!')
+  }
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <div className="form-fields">
+        <input  placeholder="E-mail" type="text" onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder="Senha" type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleOnClick}>Acessar</button>
+      </div>
+      <label>{message}</label>
+    </div>
+  );
+}
+
+export default App;
